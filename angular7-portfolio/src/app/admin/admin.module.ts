b91a5ddce1f1d178e7dde62b307c597201df3e12
citlas <citlalli.prado@gmail.com>
2019-06-11 10:15:04 +0200

    Admin- Nested Router-Outlet, Child Routes & forChild()

--- angular7-portfolio/src/app/admin/admin.module.ts
+++ angular7-portfolio/src/app/admin/admin.module.ts
@@ -4,11 +4,13 @@ import { ProjectListComponent } from './project-list/project-list.component';
 import { ProjectCreateComponent } from './project-create/project-create.component';
 import { ProjectUpdateComponent } from './project-update/project-update.component';
 import { ProjectComponent } from './project/project.component';
+import { AdminRoutingModule } from  './admin-routing.module';
 
 @NgModule({
   declarations: [ProjectListComponent, ProjectCreateComponent, ProjectUpdateComponent, ProjectComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AdminRoutingModule
   ]
 })
 export class AdminModule { }
